2c6983fc2775afc64f6732e5058338b2722299e6
Nina Shakhanova <shahanova.ns@gmail.com>
2026-01-20 19:35:58 +0300

    Создан приватный путь

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import CostAnalysis from "./components/CostAnalysis/CostAnalysis";
+import PrivateRoute from "./components/PrivateRoute";
 function AppRoutes() {
   return (
     <Routes>
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/analytics" element={<CostAnalysis />} />
+      </Route>
+
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/" element={<MainPage />} />
-      <Route path="*" element={<LoginPage />} />
-      <Route path="/analytics" element={<CostAnalysis />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
